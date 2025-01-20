d5fef5b1186db507b6ef0ba616f2e6e1bd218524
kysman2580 <kysman2580@gmail.com>
2025-01-20 11:25:41 +0900

    java workspace 등록'

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,20 @@
+// JS와 주석 동일
+
+/* Run 버튼 클릭 시 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java)
+ *	-> .class 파일(byte code) 생성
+ *
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class 파일명)
+ * 
+ */
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("ㅎㅇ");
+		System.out.println("ㅎㅇ");
+		
+	}
+
+}
